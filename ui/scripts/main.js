547104bb945bf4734477bb5ac54cabcef171c3f5
Simon Hyll <hyllsimon@gmail.com>
2023-03-20 06:21:15 +0100

    Fixed

--- ui/scripts/main.js
+++ ui/scripts/main.js
@@ -1,11 +1,18 @@
-//const { invoke } = window.__TAURI__.tauri;
+// You have to use this approach based on the way you're developing your project
+// Note that it requires '"withGlobalTauri": true' in tauri.conf.json
+const { invoke } = window.__TAURI__.tauri;
+
+// These two approaches require you to use Vite or Webpack, because the whole
+// idea of using "import" is something that's added to Javascript from bundlers
+// and aren't part of the language itself
 //import { invoke }  from '@tauri-app/api'
-import { invoke } from '../../node_modules/@tauri-apps/api'
+// import { invoke } from '../../node_modules/@tauri-apps/api'
 
 let greetInputEl;
 let greetMsgEl;
 
 async function greet() {
+  console.log('WTF')
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
 }
