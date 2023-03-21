eafdaeb7ad17fd7db6a76ba84fe6092bfc1d7491
HarrisonLeece <harryleecemail@gmail.com>
2023-03-20 17:16:26 -0700

    fixed configs, should be working

--- ui/scripts/main.js
+++ ui/scripts/main.js
@@ -3,7 +3,7 @@
 // These two approaches require you to use Vite or Webpack, because the whole
 // idea of using "import" is something that's added to Javascript from bundlers
 // and aren't part of the language itself
-import { invoke }  from '@tauri-app/api'
+import { invoke }  from '@tauri-apps/api'
 // import { invoke } from '../../node_modules/@tauri-apps/api'
 
 let greetInputEl;
